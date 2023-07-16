c4a5e77f77d012f6ee9aec224ee4c5084b49b9af
jfuentescasillas <j.fuentes.casillas@gmail.com>
2023-07-16 17:41:50 +0200

    Initial Commit

---
+++ RickAndMorty_JAFC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty_JAFC
+//
+//  Created by jfuentescasillas on 16/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
